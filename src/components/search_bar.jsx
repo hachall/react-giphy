15bd8d7055731ef4f65f7ced6b915f0146a2975a
Hamish Hall <hachall@hotmail.com>
2018-10-19 15:05:22 +0100

    experimenting with built in component methods

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      term: ""
+    }
+  }
+
   handleChange = (event) => {
+    this.setState({
+      term: event.target.value
+    });
     this.props.searchFunction(event.target.value)
   }
 
+
+
   render() {
     return (
-      <input type="text" className="form-search form-control"
+      <input
+      value={this.state.term}
+      type="text"
+      className="form-search form-control"
       onChange={this.handleChange}/>
     );
   }
